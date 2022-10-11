2f8549b0c4205de898ffe9a0a1e86f660b78469f
Govind Raj <govindr@examroom.ai>
2022-10-11 18:43:42 +0530

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,8 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
