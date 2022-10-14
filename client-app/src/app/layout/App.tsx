792d4d58f20787320448c9c5c588d7416f75d334
Govind Raj <govindr@examroom.ai>
2022-10-14 17:20:05 +0530

    end of section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,9 @@ import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashBoard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/ajent';
+import LoadingComponet from './loadingComponent';
+
 
 
 
@@ -16,10 +19,18 @@ function App() {
   const[activities,setActivities] = useState<Activity[]>([]);
   const[selectedActivity,setSelectedActivity] = useState<Activity | undefined>(undefined);
   const[editMode,setEditMode] = useState(false);
+  const[loading,setLoading] = useState(true);
+  const[submitting,setSubmitting] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activity').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+      let activities:Activity[] = [];
+      response.forEach(activity =>{
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   },[]);
 
@@ -41,29 +52,50 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity : Activity){
-    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id),activity])
-    :setActivities ([...activities,{...activity, id:uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if(activity.id){
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id),activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else{
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities ([...activities,activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
     }
 
   function handleDeleteActivity(id: string){
-    setActivities([...activities.filter(x => x.id !=id)]);
+    setSubmitting(true)
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(x => x.id !=id)]);
+      setSubmitting(false)
+    })
   }
+
+  if(loading) return <LoadingComponet content='Loading App'></LoadingComponet>
 
   return (
     <>
     <NavBar openForm={handleFormOpen}/>
     <Container style={{marginTop:'7em'}}>
-    <ActivityDashboard activities={activities}
-    selectedActivity={selectedActivity}
-    selectActivity={handleSelectedActivity}
-    cancelSelectActivity={handleCancelSelectedActivity}
-    editMode={editMode}
-    openForm={handleFormOpen}
-    closeForm={handleFormClose}
-    createOrEdit={handleCreateOrEditActivity}
-    deleteActivity={handleDeleteActivity}
+    <ActivityDashboard 
+      activities={activities}
+      selectedActivity={selectedActivity}
+      selectActivity={handleSelectedActivity}
+      cancelSelectActivity={handleCancelSelectedActivity}
+      editMode={editMode}
+      openForm={handleFormOpen}
+      closeForm={handleFormClose}
+      createOrEdit={handleCreateOrEditActivity}
+      deleteActivity={handleDeleteActivity}
+      submitting={submitting}
     />
     </Container> 
        
---
+++ client-app/src/app/layout/loadingComponent.tsx
@@ -0,0 +1,17 @@
+import { Domain } from "domain";
+import React  from "react"; 
+import { Dimmer, Loader } from "semantic-ui-react";
+
+interface Props{
+    inverted ?: boolean;
+    content ?: string;
+}
+export default function LoadingComponet({inverted = true ,content = 'Loading...'}:Props){
+    return (
+        
+            <Dimmer active={true} inverted = {inverted}>
+                <Loader content={content}/>
+            </Dimmer>
+
+    )
+}
